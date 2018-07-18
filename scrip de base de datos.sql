ff8f46156086af364c9bebfe7bbc0631f2f293ea
AlexanderG <31964961+alexandergvargas@users.noreply.github.com>
2018-07-18 10:13:52 -0500

    escrip  de baso de datos

---
+++ scrip de base de datos.sql
@@ -0,0 +1,29 @@
+use Empresa
+select * from Estudiante
+
+INSERT INTO Estudiante (idestu,nomEstu,apeestu,dniestu,edaestu,fecnacestu,nombarrioestu,nomcolegiestu,tipomodaldidadestu) 
+VALUES
+('1','Nicole Guiseppy','Salinas Fernandez','95659943','12','01/02/2000','Mala','SR.Milagros','Mañana y medio dia')
+go
+
+
+
+
+
+('Josue Daniel','Robles Venancio','995164010','73831220','DNI','Josue1@gmail.com','San Juan de Barcelona','M','26-JUN-03','A'),
+('Brayan Yorbil','Baltazar Pio','986820223','75470865','DNI','Baltazaryarvil@gmail.com','Circumbalizacion Sur Mz B2 lt17','M','15-FEB-03','A'),
+('Nicol Alexandra','Valeriano Camacho','923289261','73980548','DNI','Alexandra1@gmail.com','Calle. Los Cedros Mz E lt11 A.H San Antonio','F','29-DEC-02','A'),
+('Juan Altair','Arias Cervantes','964884979','76532608','DNI','altairPedro@outlook.com','Calle Conquistadores Mz G lt24','M','08-NOV-02','A'),
+('Rolando Edu','Soriano Trujillo','950678531','72505325','DNI','Edu1@gmail.com','Avima Mz B lt3_Mala','M','07-JAN-03','A'),
+('Steven Omar','Gutierrez Alarcon','995683993','74220083','DNI','Steven.Oga@gmail.com','Calle Juaquin Retes lt23','M','27-MAY-03','A'),
+('Angela','Arias Salinas','988895237','71468306','DNI','ariasangela535@gmail.com','Prolongacion Swayne S/N Mala','F','30-SEP-03','A'),
+('Melisa Fernanda','Reina Tomas','964871661','77272299','DNI','Melisa1@gmail.com','Jiron Real 343','F','11-DEC-02','A'),
+('Juan Manuel','Vega Cjuno','965258442','73657110','DNI','Juan1@gmail.com','Las Begoñas Mz lt7 Avima-Mala','M','19-DEC-02','A'),
+('Hilary Norma','Garridos Riveros','971343300','60500434','DNI','hilaryvale05@gmail.com.com','Jiron Real 561','F','21-OCT-03','A'),
+('Daysi Giulianna','Figueroa Quispe','934877592','71994637','DNI','Daysi1@gmail.com','AA.HH Santa Rosa Calle Progreso Mz lt12','F','16-APR-03','A'),
+('Cynthia Vanessa','Colan Torres','968040912','77570582','DNI','Herbert_Colan@Hotmail.com','Jiron Real 471','F','07-JUN-04','A'),
+('Jonathan Frank','Valdiglesias Castillon','989974283','75329882','DNI','valdiglesias@hotmail.com','Panamericana Antigua','M','23-AUG-04','A'),
+('Ruth Esteffany','Valeriano Arias','990401520','7930046','DNI','Esteffany1@gmail.com','Calle 5 de Diciembre_Dignidad Nacional','F','09-JAN-04','A'),
+('Alexandro Diego','Prado Castillo','936444575',null,null,'Alexandro1@gmail.com','Jiron Swayne S/N DPTO E','M','12-MAY-04','A'),
+('Alexsief Maykol','Vivas Sorianio','978083249','72842765','DNI','alexsief-08@hotmail.com','Calle las Begoñas Mz J lt17 Avima_Mala','M','12-JUL-03','A'),
+('Ana Cristina','Huaman Huaman','925498311','72164915','DNI','Ana1@gmail.com','Calle los Laureles','F','13-MAY-03','A'),
